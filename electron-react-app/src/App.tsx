06f878a476bd9557bcfa48df3485ccbec75d0d93
박지수 Jisu Park <qkrwltn8976@dongguk.edu>
2020-07-01 10:46:55 +0900

    typescript test

--- electron-react-app/src/App.tsx
+++ electron-react-app/src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
+import { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ChatList } from './app/pages'
+import { render } from '@testing-library/react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <Fragment>
+        <h1>테스트</h1>
+        <ChatList name = "랩소디"/>
+      </Fragment>
+    );
 }
 
 export default App;
---
+++ electron-react-app/src/app/pages/ChatRoom/Bot/index.tsx
@@ -0,0 +1,11 @@
+import React, { Component, Fragment } from 'react';
+
+function BotChatRoom() {
+    return (
+        <div>
+            <h3>채팅방리스트</h3>
+        </div>
+    )
+}
+
+export default BotChatRoom;
