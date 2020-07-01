aeea4ba93a71a1e2b74607256c8beed425b582c7
박지수 Jisu Park <qkrwltn8976@dongguk.edu>
2020-07-01 16:43:09 +0900

    added stompjs script

--- electron-react-app/src/App.tsx
+++ electron-react-app/src/App.tsx
@@ -2,14 +2,16 @@ import * as React from 'react';
 import { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { ChatList } from './app/pages'
+import { ChatList, BotChatRoom } from './app/pages'
 import { render } from '@testing-library/react';
 
 function App() {
+    const script = document.createElement('script');
     return (
-      <Fragment>
+      <Fragment>   
         <h1>테스트</h1>
         <ChatList name = "랩소디"/>
+        <BotChatRoom />
       </Fragment>
     );
 }
--- electron-react-app/src/app/pages/ChatRoom/Bot/index.tsx
+++ electron-react-app/src/app/pages/ChatRoom/Bot/index.tsx
@@ -1,11 +1,22 @@
 import React, { Component, Fragment } from 'react';
+import stompjs from '@stomp/stompjs';
 
-function BotChatRoom() {
-    return (
-        <div>
-            <h3>채팅방리스트</h3>
-        </div>
-    )
+class BotChatRoom extends Component {
+    componentDidMount() {
+        const script = document.createElement("script");
+
+        script.src = "https://cdnjs.cloudflare.com/ajax/libs/stomp.js/2.3.3/stomp.js";
+        script.async = true;
+
+        document.body.appendChild(script);
+    }
+    render() {
+        return (
+            <div>
+                <h3>채팅방리스트</h3>
+            </div>
+        )
+    }
 }
 
 export default BotChatRoom;
