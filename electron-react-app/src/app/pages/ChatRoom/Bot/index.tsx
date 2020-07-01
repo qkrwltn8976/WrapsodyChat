17db0f92f78b105a808fae756cc3b7d71e2d97c7
박지수 Jisu Park <qkrwltn8976@dongguk.edu>
2020-07-01 18:10:43 +0900

    stomp websocket connection test

--- electron-react-app/src/app/pages/ChatRoom/Bot/index.tsx
+++ electron-react-app/src/app/pages/ChatRoom/Bot/index.tsx
@@ -1,14 +1,67 @@
 import React, { Component, Fragment } from 'react';
-import stompjs from '@stomp/stompjs';
+import { Client } from "@stomp/stompjs";
+interface Props {
+    client: Client,
+    name: String,
+}
 
-class BotChatRoom extends Component {
+interface State { user: [], message: [] }
+// const StompElement = (
+//     <script>
+//         var ws = new WebSocket('ws://127.0.0.1:15674/ws');
+//         var client = Stomp.over(ws);
+//         var on_connect = function() {
+//             console.log(`connected`)
+//         };
+//         var on_error =  function() {
+//             console.log(`error`)
+//         };
+//         client.connect('guest', 'guest', on_connect, on_error, '/');
+//     </script>
+// )
+
+class BotChatRoom extends React.PureComponent {
+    client: any = "";
+    name: String = "";
+    state: any = {user: [], message: []};
+    constructor(props: {client: any, name: string;}, state: {}) {
+        super(props, state);
+        this.client = new Client();
+        this.client.brokerURL = "ws://192.168.100.30:9099/";
+        this.client.onConnect = () => {
+            console.log("성공")
+            // this.client.subscribe("/topic/user", f => {
+            //     this.setState({
+            //         ...this.state,
+            //         user: JSON.parse(f.body)
+            //     });
+            // });
+
+            this.client.publish({
+                destination: `/app/init`,
+                body: ""
+            });
+        };
+        
+        this.client.activate();
+    }
     componentDidMount() {
-        const script = document.createElement("script");
+        let client: String;
+        let name: String;
+        let state = { user: [], message: [] };
+        // const script = document.createElement("script");
+        // const stomp = document.createElement("script");
+        // // stomp.id = "stomp-script"
+        // script.src = "https://cdnjs.cloudflare.com/ajax/libs/stomp.js/2.3.3/stomp.js";
 
-        script.src = "https://cdnjs.cloudflare.com/ajax/libs/stomp.js/2.3.3/stomp.js";
-        script.async = true;
+        // var ws = new WebSocket('ws://127.0.0.1:15674/ws');
+        // var client = Stomp.client("ws://localhost:61614/stomp");
+        // // script.async = true;
+        // // stomp.innerHTML =
+        // //     "var ws = new WebSocket('ws://127.0.0.1:15674/ws');var client = Stomp.over(function(){return new SockJS(ws);});var on_connect = function() {console.log(`connected`)};var on_error =  function() {console.log(`error`)};client.connect('guest', 'guest', on_connect, on_error, '/');";
+        // document.body.appendChild(script);
 
-        document.body.appendChild(script);
+        // document.body.appendChild(stomp);
     }
     render() {
         return (
