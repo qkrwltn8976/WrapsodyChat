c74350b0bd4ba70c1640f57f29cdbd0b7499db7f
sunaji <jisuna314@gmail.com>
2020-07-03 18:01:33 +0900

    first week

--- electron-react-app/src/app/components/MemberList.tsx
+++ electron-react-app/src/app/components/MemberList.tsx
@@ -15,12 +15,27 @@ class MemberList extends React.Component<Props>{
                 <div>
 
                 </div>
-
             );
         }else if(memberListType == 'select'){
             return(
                 <div>
-                    
+                    <li className="selected" expand-on-hover="false">
+                    <div className="organ_wrapper">
+                        <span ng-style="node.type === 'dept' &amp;&amp; !node.hasChildren &amp;&amp; {'visibility': 'hidden'}">
+                            <input type="checkbox" id="member-adminobject:1515" disabled="disabled" checked="checked">
+                            <label htmlFor="member-adminobject:1515" data-nodrag="">
+                                <i className="icon_checkbox disabled"></i>
+                            </label>
+                        </span>
+                        <div wrapmsgr-user-profile="users[node.value] || node.value" user-profile-disabled="node.type === 'dept'">
+                            <span className="user-photo no-photo cyan" user="{userName: node.columnText, userId: node.value, type: node.type}" "="">ad</span>
+                            <span className="wrapmsgr_member">administrator</span>
+                        </div>
+                        <span className="wrapmsgr_master">Owner</span>
+                        <ol ui-tree-nodes="">
+                        </ol>
+                    </div>
+			        </li>
                 </div>
             );
         }else if(memberListType == 'selected'){
