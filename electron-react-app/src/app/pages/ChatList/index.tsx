7e96b3b607017f21f5cb826ce12cedbf8dafabaf
bomi <bomi2006@naver.com>
2020-07-03 17:22:24 +0900

    absolute path

--- electron-react-app/src/app/pages/ChatList/index.tsx
+++ electron-react-app/src/app/pages/ChatList/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import Chat from "./Chat";
+import Chat from "../../components/Chat";
 import SearchBar from "../../components/SearchBar";
-import {SearchType} from "../../../libs/enum-type"
+import {SearchType} from "@libs/enum-type"
 
 function ChatList() {
     return (
