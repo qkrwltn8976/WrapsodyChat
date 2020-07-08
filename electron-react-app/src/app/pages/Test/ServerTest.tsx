991a667bb91e92e3f3048a25954c3d8863ae51d2
sunaji <jisuna314@gmail.com>
2020-07-08 17:13:49 +0900

    test

--- electron-react-app/src/app/pages/Test/ServerTest.tsx
+++ electron-react-app/src/app/pages/Test/ServerTest.tsx
@@ -1,8 +1,9 @@
 import React, { Component, Fragment } from 'react';
-import { Client, Message, StompSubscription, Stomp, StompConfig } from "@stomp/stompjs";
+import { Client, IMessage, StompSubscription, Stomp, StompConfig } from "@stomp/stompjs";
 import { createClient, subscribe, publish } from 'src/libs/stomp';
 import { rejects } from 'assert';
 import { resolve } from 'url';
+import { v4 } from 'uuid';
 
 interface Props {
     client: Client,
@@ -18,12 +19,22 @@ class ServerTest extends React.PureComponent {
         super(props, state);
 
         const client = createClient("admin", "1111");
+        var callback = function(message: IMessage) {
+            // called when the client receives a STOMP message from the server
+            if (message.body||message.isBinaryBody||message.command) {
+               var obj = JSON.parse(message.body)
+               console.log(obj)
+               console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa");
+            }
+            else
+              console.log("dddddddddddddddddddddddddddddddddddddddddd");
+        };
+        var queue:string = "user-"+ "admin-"+v4();
 
         client.onConnect = function () {
             console.log("connected to Stomp");
-
             subscribe(client, 'admin', '98f7e404-f6b7-4513-84b4-31aa1647bc6d', (payload: {}) => { console.log(payload) });
-
+            console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@subscribe@@@@@@@@@@@@@@@@@@@@@@@@@@");
             publish(client, 'api.user.info', 'admin', '98f7e404-f6b7-4513-84b4-31aa1647bc6d', {});
             publish(client, 'api.conversation.list', 'admin', '98f7e404-f6b7-4513-84b4-31aa1647bc6d', {});
 
