def295f8f2319ac0f21386fa15d26b0a1228d47d
박지수 Jisu Park <qkrwltn8976@dongguk.edu>
2020-07-08 13:29:12 +0900

    stomp module

--- electron-react-app/src/app/pages/Test/ServerTest.tsx
+++ electron-react-app/src/app/pages/Test/ServerTest.tsx
@@ -1,5 +1,8 @@
 import React, { Component, Fragment } from 'react';
 import { Client, Message, StompSubscription, Stomp, StompConfig } from "@stomp/stompjs";
+import {createClient, subscribe, publish} from 'src/libs/stomp';
+import { rejects } from 'assert';
+import { resolve } from 'url';
 
 interface Props {
     client: Client,
@@ -14,68 +17,19 @@ class ServerTest extends React.PureComponent {
     constructor(props: {}, state: {}) {
         super(props, state);
 
-
-
-        const client = new Client({
-            brokerURL: "ws://192.168.100.30:9500/ws",
-            connectHeaders: {
-                login: "admin",
-                passcode: "1111",
-                host: "/wrapsody-oracle",
-            },
-            debug: function (str) {
-                console.log(str);
-            },
-            reconnectDelay: 500000,
-            heartbeatIncoming: 100000,
-            heartbeatOutgoing: 100000
-        });
-
-
-        // client.onReceive = function() {
-
-        // }
-
-        // var callback = function (message: Message) {
-        //     // called when the client receives a STOMP message from the server
-        //     if (message.body || message.isBinaryBody || message.command) {
-        //         console.log("got message with body " + message.body)
-        //     }
-        //     console.log("got empty message");
-        // };
+        const client = createClient("admin", "1111");
 
         client.onConnect = function () {
             console.log("connected to Stomp");
-            let callback = function (message: Message) {
-                // called when the client receives a STOMP message from the server
-                if (message.body || message.isBinaryBody || message.command) {
-                    console.log("got message with body " + message.body)
-                }
-                console.log("got empty message");
-            };
 
-            client.watchForReceipt("test", function(frame) {
-                console.log('Receipt: ', frame);
-            })
-            client.subscribe("/exchange/user-admin", callback, { "x-queue-name": "user-admin-98f7e404-f6b7-4513-84b4-31aa1647bc6d" });
+            subscribe(client, 'admin', '98f7e404-f6b7-4513-84b4-31aa1647bc6d', (payload:{})=>{console.log(payload)});
 
-            let body = { senderId: "admin", locale: "ko-KR", payload: {} };
-            client.publish({ 
-                destination: "/exchange/request/api.user.info", 
-                body: JSON.stringify(body), 
-                headers: { "reply-to": "/temp-queue/api.user.info", "content-type": "application/json" } 
-            });
-            //  "content_encoding": "UTF-8", "__TypeId__": "com.wrapsody.messaging.model.ApiRequest", "receipt":"test"
-            //     var locale = navigator.language || navigator.userLanguage || 'ko-kr';
+            publish(client, 'api.user.info', 'admin', '98f7e404-f6b7-4513-84b4-31aa1647bc6d', {});
+            publish(client, 'api.conversation.list', 'admin', '98f7e404-f6b7-4513-84b4-31aa1647bc6d', {});
 
-            // $scope.client.send(
-            // 		"/exchange/request/" + api, 
-            // 		{"reply-to": "/temp-queue/" + api, "content-type" : "application/json"}, 
-            // 		JSON.stringify({senderId : $scope.user.id, locale: locale, payload : payload}));
         }
 
         client.onStompError = function () {
-
         }
 
         client.activate();
--- electron-react-app/src/libs/stomp.tsx
+++ electron-react-app/src/libs/stomp.tsx
@@ -1,3 +1,53 @@
-import { Stomp } from "@stomp/stompjs";
+import { Client, IMessage } from "@stomp/stompjs";
 
-export default Stomp;
+export function createClient(login: string, passcode: string) {
+    return new Client({
+        brokerURL: "ws://192.168.100.30:9500/ws",
+        connectHeaders: {
+            login,
+            passcode,
+            host: "/wrapsody-oracle",
+        },
+        debug: function (str) {
+            console.log(str);
+        },
+        reconnectDelay: 500000,
+        heartbeatIncoming: 100000,
+        heartbeatOutgoing: 100000,
+    })
+}
+
+export function subscribe(client: Client, userId: string, uuid: string, callback: any) {
+    let obj;
+    client.subscribe(`/exchange/user-${userId}`, (message:IMessage) =>  {
+        if (message.body || message.isBinaryBody || message.command) {        
+            obj = JSON.parse(message.body);
+            // callbackify(obj);
+            // console.log(obj)
+            callback(obj.payload)
+        }
+        else {
+            obj = {}
+            console.log("got empty message");
+        }
+        
+            
+        return obj;
+    }, {
+        "x-queue-name": `user-${userId}-${uuid}`
+    });
+
+    console.log(obj);
+    return obj;
+}
+
+export function publish(client: Client, api: string, userId: string, uuid: string, payload: {}) {
+    client.publish({
+        destination: `/exchange/request/${api}`,
+        body: JSON.stringify({
+            senderId: userId, locale: "ko-KR", payload,
+        }),
+        headers: { "reply-to": `user-admin-${uuid}`, "content-type": "application/json", "correlation_id ": api }
+    })
+}
+
