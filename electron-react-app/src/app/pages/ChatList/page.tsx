8b4fedd2ade99b5be2460e6406160fa03769e1fa
bomi <bomi2006@naver.com>
2020-07-10 09:36:29 +0900

    use useState

--- electron-react-app/src/app/pages/ChatList/page.tsx
+++ electron-react-app/src/app/pages/ChatList/page.tsx
@@ -2,6 +2,7 @@ import {Component, Fragment} from 'react';
 import React from 'react';
 import { createClient, subscribe, publish } from 'src/libs/stomp';
 import ReactDOM from 'react-dom';
+import {v4} from "uuid"
 
 class ChatPage extends Component{
     roomName:[] =[];
@@ -11,17 +12,18 @@ class ChatPage extends Component{
     client: any;
     payload: any;
     state: any = { payload: [] };
+    
 
 
     stompConnection = () => {
             this.client = createClient("admin", "1111");
-
+            let queue = v4()
             let obj = {};
             
             this.client.onConnect = () => {
                 console.log("connected to Stomp");
     
-                subscribe(this.client, 'admin', '98f7e404-f6b7-4513-84b4-31aa1647bc6d', (payload: any) => {
+                subscribe(this.client, 'admin', queue, (payload: any) => {
                     this.state = ({
                         ...this.state,
                         payload: payload
@@ -43,7 +45,6 @@ class ChatPage extends Component{
             </document-icon>
             <div className = "title_5" id = "title_5">
                     <span className = "chatroom-name ng-binding">{item.name}</span>
-                    <span className="chatroom-user-cnt ng-binding">2</span>
                     <span className="chatroom-user-cnt ng-binding">{item.memberCount}</span>
                 <i></i>
                     <span className = "chatroom-message-contents ng-binding">{item.latestMessage}</span>
@@ -59,7 +60,7 @@ class ChatPage extends Component{
         }</Fragment>, document.getElementById('chatList'));
                     
                 });
-                publish(this.client, 'api.conversation.list', 'admin', '98f7e404-f6b7-4513-84b4-31aa1647bc6d', {});
+                publish(this.client, 'api.conversation.list', 'admin', queue, {});
 
                 
             }
--- electron-react-app/src/app/pages/index.tsx
+++ electron-react-app/src/app/pages/index.tsx
@@ -2,3 +2,4 @@ export { default as ChatList } from './ChatList';
 export { default as BotChatRoom } from './ChatRoom/Bot';
 export { default as DocumentChatRoom } from './ChatRoom/Document';
 export { default as ChatPage } from './ChatList/page'
+export {default as ServerTest} from './Test/ServerTest'
