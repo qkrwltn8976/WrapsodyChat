6aff926861e6eada4800220bfc164efe82ded01f
박지수 Jisu Park <qkrwltn8976@dongguk.edu>
2020-07-22 10:31:10 +0900

    botcommand api

---
+++ electron-react-typescript-base-proj/src/renderer/models/BotCommand.tsx
@@ -0,0 +1,10 @@
+export type BotCommand = {
+    botUserId: string,
+    catalogOrder: number
+    command: string,
+    createdAt: number
+    intentId: number,
+    intentType: number,
+    priority: number,
+    updatedAt: number
+}
