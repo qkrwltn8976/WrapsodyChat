bfda1f6229d877d560b4e51455565f22564fd16e
sunaji <jisuna314@gmail.com>
2020-07-27 10:12:39 +0900

    add footer

--- electron-react-typescript-base-proj/src/renderer/app/components/Footer.tsx
+++ electron-react-typescript-base-proj/src/renderer/app/components/Footer.tsx
@@ -1,9 +1,22 @@
 import * as React from 'react';
 
+const remote = require('electron').remote
+
+var closeWindow = (event:any)=>{
+    var win = remote.getCurrentWindow()
+    win.close()
+}
+
 function Footer() {
+    
+
     return (
-        <div></div>
+        <div className="wrapmsgr_popup_footer">
+            <input type="submit" className="wrapmsgr_button primary wrapmsgr_right" value="OK" ng-disabled="!loggedIn" />
+            <input type="button" className="wrapmsgr_button wrapmsgr_right" value="Cancel" ng-disabled="!loggedIn" ng-click="hidePopup($event)" onClick = {closeWindow} />
+        </div>
     );
 }
 
 export default Footer;
+
