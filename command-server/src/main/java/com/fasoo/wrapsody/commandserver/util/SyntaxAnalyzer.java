7deef2f73ab8c6e8ba89c8e43be229bb611fc6cf
SeoBomi <bomi2006@naver.com>
2020-07-29 17:48:34 +0900

    edit deadline date process

--- command-server/src/main/java/com/fasoo/wrapsody/commandserver/util/SyntaxAnalyzer.java
+++ command-server/src/main/java/com/fasoo/wrapsody/commandserver/util/SyntaxAnalyzer.java
@@ -29,12 +29,15 @@ public class SyntaxAnalyzer {
 
     public SyntaxAnalyzer(String message) throws ParseException {
         //메세지가 '@' 으로 시작하면 구문분석한다
-        if(message.indexOf(0)=='@') {
+//        System.out.println(message);
+        if(message.charAt(0)=='@') {
             this.message = message;
 
             //기호 '@' 제거 후 공백문자로 split 하여 배열 cmd에 저장
             message = message.substring(1);
             cmd = message.split(" ");
+            System.out.println("자른다");
+            System.out.println(message);
 
             //첫번째 단어가 Command 열거형에 있는 name 중 하나와 일치하면 type에 명령어를 저장하고 iscommand를 참으로 반환한다
             for (Command command : Command.values()) {
